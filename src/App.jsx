de982bad76dcd1e6c74cf60c9eededc788d0c9e4
Aleksandra <aleksandrainca@gmail.com>
2025-05-22 20:53:22 -0400

    Validation complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,40 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './App.module.css';
+import * as yup from 'yup';
+
+const EmailValidationSchema = yup
+	.string()
+	.matches(
+		/^[\w@.]*$/,
+		'Email введен не верно, используйте только буквы, цифры и нижнее подчеркивание',
+	)
+	.max(20, 'Email должен содержать не больше 20 символов.');
+
+const passwordValidationSchema = yup
+	.string()
+	.matches(
+		/^[\w@.]*$/,
+		'Пароль введен не верно.Доступные символы- буквы, цифры,точка, @.',
+	)
+	.max(10, 'Пароль не может содержать больше чем 10 символов.');
+
+// const passcheckValidationSchema = yup
+// 	.string()
+// 	.oneOf([yup.ref('password'), null], 'Пароли не совпадают.');
+
+// const passwordBluerSchema = yup
+// 	.string()
+// 	.min(3, 'Пароль не может содержать меньше 3 символов');
+
+const validateAndGetErrorMessage = (schema, value) => {
+	let errorMessage = null;
+	try {
+		schema.validateSync(value, { abortEarly: false });
+	} catch ({ errors }) {
+		errorMessage = errors[0];
+	}
+	return errorMessage;
+};
 
 const initialState = {
 	email: '',
@@ -24,31 +59,75 @@ const sendData = (formData) => {
 
 export const App = () => {
 	const { getState, updateState } = useStore();
+	const [emailError, setEmailError] = useState(null);
+	const [passwordError, setPasswordError] = useState(null);
+	const [passcheckError, setPasscheckError] = useState(null);
+	const [isFormValid, setIsFormValid] = useState(false);
+
+	const { email, password, passcheck } = getState();
+
+	useEffect(() => {
+		const validateForm = () => {
+			const emailErr = validateAndGetErrorMessage(EmailValidationSchema, email);
+			const passwordErr = validateAndGetErrorMessage(
+				passwordValidationSchema,
+				password,
+			);
+
+			setEmailError(emailErr);
+			setPasswordError(passwordErr);
+
+			setIsFormValid(!emailErr && !passwordErr);
+		};
+		validateForm();
+	}, [email, password, passcheck]);
 
 	const onSubmit = (event) => {
 		event.preventDefault();
-		sendData(getState());
-	};
+		let passcheckErr = null;
+		if (password && passcheck) {
+			if (password !== passcheck) {
+				passcheckErr = 'Пароли не совпадают.';
+			}
+		}
 
-	const { email, password, passcheck } = useStore();
+		setPasscheckError(passcheckErr);
+		setIsFormValid(!passcheckErr);
+
+		if (!passcheckErr) {
+			sendData(getState());
+		}
+	};
 
 	return (
 		<div className={styles.app}>
-			<form onSubmit={onSubmit}>
+			<form className={styles.form} onSubmit={onSubmit}>
+				{emailError && <div className={styles.errorLabel}>{emailError}</div>}
+
 				<input
 					type="email"
 					name="email"
 					value={email}
 					placeholder="Введите вашу почту"
-					onChange={({ target }) => updateState('email', target.value)}
+					onChange={({ target }) => {
+						updateState('email', target.value);
+					}}
 				/>
+				{passwordError && (
+					<div className={styles.errorLabel}>{passwordError}</div>
+				)}
 				<input
 					type="password"
 					name="password"
 					value={password}
 					placeholder="Введите пароль"
-					onChange={({ target }) => updateState('password', target.value)}
+					onChange={({ target }) => {
+						updateState('password', target.value);
+					}}
 				/>
+				{passcheckError && (
+					<div className={styles.errorLabel}>{passcheckError}</div>
+				)}
 				<input
 					type="password"
 					name="passcheck"
@@ -56,10 +135,10 @@ export const App = () => {
 					placeholder="Повторите пароль"
 					onChange={({ target }) => updateState('passcheck', target.value)}
 				/>
-				<button type="submit">Зарегестрироваться</button>
+				<button type="submit" disabled={!isFormValid}>
+					Зарегестрироваться
+				</button>
 			</form>
 		</div>
 	);
 };
-
-export default App;
